956fb7b2e52e4e39d5a0d831cfa3f8398195ab80
Nick Mayhall <nmayhall@vt.edu>
2024-02-27 17:57:09 -0500

    added q

--- hexabenzocoronene/sto3g/correlation/plot_correlations.jl
+++ hexabenzocoronene/sto3g/correlation/plot_correlations.jl
@@ -6,12 +6,21 @@ using Printf
 
 
 function run()
-    @load("/Users/arnab/arnab/workspace/2024_faraday_discussions_data/hexabenzocoronene/sto3g/correlation/tucker_thresh_5_2e4.jld2")
+    @load("tucker_thresh_5_2e4.jld2")
+    cmf_state = TPSCIstate(clusters, FockConfig(init_fspace))
 
 
     display(v0b)
-    n1, n2, sz1, sz2 = correlation_functions(v0b)
+    cf = correlation_functions(v0b, cmf_state)
+    n1 = cf["N"][1]
+    n2 = cf["N"][2]
+    sz1 = cf["Sz"][1]
+    sz2 = cf["Sz"][2]
+    q1 = cf["Q"][1]
+    q2 = cf["Q"][2]
+    
     display(n2[1])
+
     max_val = max(0, maximum(abs.(n2[1])))
 
     plotd = heatmap(n2[1]; color=palette(:RdGy_9, 100), aspect_ratio=1, dpi=300, size=(300,300), right_margin = 10Plots.mm,  
@@ -26,7 +35,9 @@ function run()
     hline!(0.5:(m+0.5), c=:grey, label=false)
 
 
-    savefig(plotd,@sprintf("/Users/arnab/arnab/workspace/2024_faraday_discussions_data/hexabenzocoronene/sto3g/correlation/n_correlation_hbc.png"))
+    savefig(plotd,@sprintf("n_correlation_hbc.png"))
+    
+
     max_val1 = max(0, maximum(abs.(sz2[1])))
     plotd = heatmap(sz2[1]; color=palette(:RdGy_9, 100), aspect_ratio=1, dpi=300, size=(300,300), right_margin = 10Plots.mm,  
                         clims=(-max_val1, max_val1), ticks = false,xaxis=false,yaxis=false, 
@@ -36,7 +47,19 @@ function run()
     hline!(0.5:(m+0.5), c=:grey, label=false)
 
 
-    savefig(plotd,@sprintf("/Users/arnab/arnab/workspace/2024_faraday_discussions_data/hexabenzocoronene/sto3g/correlation/sz_correlation_hbc.png"))
+    savefig(plotd,@sprintf("sz_correlation_hbc.png"))
+    
+
+    max_val1 = max(0, maximum(abs.(q2[1])))
+    plotd = heatmap(q2[1]; color=palette(:RdGy_9, 100), aspect_ratio=1, dpi=300, size=(300,300), right_margin = 10Plots.mm,  
+                        clims=(-max_val1, max_val1), ticks = false,xaxis=false,yaxis=false, 
+                        xlims = (0.5,7.5), ylims = (0.5,7.5),yflip=true)
+    m, n = size(q2[1])
+    vline!(0.5:(n+0.5), c=:grey, label=false)
+    hline!(0.5:(m+0.5), c=:grey, label=false)
+
+
+    savefig(plotd,@sprintf("q_correlation_hbc.png"))
 
 end
 
