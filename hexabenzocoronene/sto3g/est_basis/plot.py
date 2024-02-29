8d2e8d4b36ff739ade8f21e0b40a3754b731a31e
arnab82 <arnab22@vt.edu>
2024-02-29 11:27:48 -0500

    some figures are modified

--- hexabenzocoronene/sto3g/est_basis/plot.py
+++ hexabenzocoronene/sto3g/est_basis/plot.py
@@ -7,22 +7,22 @@ matrix = np.array([[-2.071532588, -2.065048656, -2.06626555],
 
 num_rows, num_cols = matrix.shape
 
-x = np.arange(1, num_rows+1)
+x = np.arange(2, num_rows+2,1)
 width = 0.2
 
 fig, ax = plt.subplots()
-ax.set_ylim(0, -3.2)
+ax.set_ylim(0, -3.5)
 
 
 for i in range(num_cols):
-    ax.bar(x + i*width, matrix[:, i], width, label=f'${{\delta e^{{-1}}}}={i+1}$')
+    ax.bar(x + i*width, matrix[:, i], width, label=f'${{\delta_{{elec}}}}={i+1}$')
 
 
 ax.set_ylabel('Correlation Energy (eV)')
-ax.set_xlabel("Nbody")
+ax.set_xlabel("nbody")
 # ax.set_title("Contribution of nbody terms with the ${\delta e^{-1}}$ to the correlation energy")
 ax.set_xticks(x)
-ax.legend(fontsize=12)
+ax.legend(fontsize=12, loc='upper left')
 
 # Add bar labels
 for i in range(num_rows):
@@ -30,7 +30,7 @@ for i in range(num_rows):
         ax.text(x[i] + (j+0.5)*width, matrix[i, j], f'{matrix[i, j]:.4f}', ha='right', va='bottom',rotation=90)
 fig.set_dpi(300)
 fig.set_size_inches(5,5)
-fig.savefig('nbody_correlations.png')
+fig.savefig('nbody_correlations.jpg', bbox_inches='tight', dpi=300)
 plt.show()
 
 
